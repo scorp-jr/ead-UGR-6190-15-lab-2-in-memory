45498f0f2c7dba7571961c686020d2cfcd82b78a
scorp_jr <biruk.scorpion.dereje@gmail.com>
2025-11-28 22:09:08 +0300

    GET POST PUT DELETE

---
+++ PizzaStore/PizzaDb.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,6 +1,73 @@
+using Microsoft.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<PizzaDb>(
+options => options.UseInMemoryDatabase("pizzas")
+);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStore API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
 
+// GET
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+// POST
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+// GET by Id
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) =>
+await db.Pizzas.FindAsync(id));
+
+// PUT
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id)
+=>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+// DELETE
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 app.Run();
